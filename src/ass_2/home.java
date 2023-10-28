b0c723fe4d71277ec0379ce03358947627b0e006
gouri-agrawal <gouri.agrawal2020@vitstudent.ac.in>
2023-10-28 22:04:33 +0530

    first commit

---
+++ src/ass_2/home.java
@@ -0,0 +1,13 @@
+package ass_2;
+
+public class home{
+	public static void main(String[] args) {
+		String h = "yes";
+		if(h=="yes") {
+			System.out.print("nice");
+		}else {
+			System.out.print("nice");
+		}
+	}
+	
+}
